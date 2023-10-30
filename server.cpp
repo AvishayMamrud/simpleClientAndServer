ad15cbbbf8c0aa519a3b5f2bd1d08e87039f6c69
mamrud <mamrud@post.bgu.ac.il>
2023-10-31 00:23:23 +0200

    more experimenting

--- server.cpp
+++ server.cpp
@@ -6,14 +6,15 @@
 #include <iostream> // For cout
 #include <unistd.h> // For read
 #include <pthread.h>
+#include <fstream>
 
 using namespace std;
 
-// typedef struct conn
-// {
-//   int connection;
-//   int sockfd;
-// } conn;
+typedef struct conn
+{
+  int connection;
+  int sockfd;
+} conn;
 
 
 void* clientReception(void*);
@@ -61,13 +62,6 @@ void* clientReception(void* nul){
   // Grab a connection from the queue
   auto addrlen = sizeof(sockaddr);
   while(true){
-    cout << sockaddr.sin_family << " ?= "
-    << AF_INET << "\n"
-    << sockaddr.sin_addr.s_addr << " ?= "
-    << htonl(INADDR_ANY) << "\n"
-    << sockaddr.sin_port << " ?= "
-    << htons(9999) << endl;
-
     int connection = accept(serv_sock, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
     if (connection < 0) {
       cout << "Failed to grab connection. errno: " << errno << endl;
@@ -76,13 +70,17 @@ void* clientReception(void* nul){
     
     cout << "A client has connected...\n" << endl;
 
-    int* con = (int*)malloc(sizeof(int));
-    *con = connection;
+    // int* con = (int*)malloc(sizeof(int));
+    // *con = connection;
+
+    conn con;
+    con.connection = connection;
+    con.sockfd = serv_sock;
 
     cout << "connection - " << connection << endl;
 
     pthread_t t;
-    int res = pthread_create(&t, NULL, &readAndRespond, (void*)con);
+    int res = pthread_create(&t, NULL, &readAndRespond, (void*)&con);
     cout << "clientReception res - " << res << endl;
   }
   close(serv_sock);
@@ -90,31 +88,65 @@ void* clientReception(void* nul){
 }
 
 void* readAndRespond(void* con){
-  int connection = *(int*)con;
-  free(con);
+  // int connection = *(int*)con;
+  int connection = ((conn*)con)->connection;
+  // free(con);
+
+  ifstream::pos_type size;
+  char * memblock;
+
   // Read from the connection
-  int maxlen = 100;
+  int maxlen = 4000;
   char buffer[maxlen];
   while(true){
+    // int error = 0;
+    // socklen_t len = sizeof (error);
+    // int retval = getsockopt(((conn*)con)->connection, SOL_SOCKET, SO_ERROR, &error, &len);
+
+    // if (retval != 0) {
+    //     /* there was a problem getting the error code */
+    //     fprintf(stdout, "error getting socket error code: %s\n", strerror(retval));
+    //     break;
+    // }
+
+    // if (error != 0) {
+    //     /* socket has a non zero error status */
+    //     fprintf(stdout, "socket error: %s\n", strerror(error));
+    //     break;
+    // }
     int bytesRead = read(connection, buffer, maxlen - 1);
-    if(bytesRead == -1){
+    if(bytesRead <= 0){ // if read returns 0, it indicates that the connection is lost (probably by closing the client's socket)
         cout << "error reading" << endl;
         break;
     }
 
     buffer[bytesRead] = '\0';
-    while (bytesRead > 0 && (buffer[bytesRead-1] == '\n' || buffer[bytesRead-1] == '\r')) buffer[--bytesRead] = '\0';
+    // while (bytesRead > 0 && (buffer[bytesRead-1] == '\n' || buffer[bytesRead-1] == '\r')) buffer[--bytesRead] = '\0';
     
-    cout << "The message was: " << buffer << endl;
+    cout << "The message was: " << bytesRead << " - " << buffer << endl;
 
     // Send a message to the connection
-    string response = "what the hell is - " + (string)buffer + '\n';
-    // cout << "response.size() - " + to_string(response.size()) << endl;
+
+    ifstream file ("html.html", ios::in|ios::binary|ios::ate);
+    if (!file.is_open())
+    {
+      break;
+    }
+    size = file.tellg();
+    memblock = new char[size];
+    file.seekg(0, ios::beg);
+    file.read(memblock, size);
+    file.close();
+
+    cout << "the complete file content is in memory" << endl;
+
+    string str = memblock;
+    string response = "HTTP/1.0 200 OK\r\n\r\n" + str;//"what the hell is - " + (string)buffer + '\n';
+    delete[] memblock;
+    
+    cout << endl << response << endl << endl;
     send(connection, response.c_str(), response.size(), 0);
 
-    // cout << "str compare - " << (const char*)"quit" << " ?= " << (const char*)buffer << " = " << strcmp((const char*)"quit", (const char*)buffer) << endl;
-    // cout << "str length - " << strlen((const char*)"quit") << " ?= " << strlen((const char*)buffer) << endl;
-    // cout << (int)(buffer[0]) << (int)(buffer[1]) << (int)(buffer[2]) << (int)(buffer[3]) << (int)(buffer[4]) << " " << (int)(buffer[5]) << " " << (int)(buffer[6]) << endl;
     if(((string)"quit").compare((string)buffer) == 0){
       cout << "quitting..." << endl;
       break;
